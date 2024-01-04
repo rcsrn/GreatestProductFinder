10f465f41be790c30f2814b7e5088491c867c8a0
casarin <nsgbanks@ciencias.unam.mx>
2024-01-03 21:31:59 -0600

    Parsing of chars to integers done in ProductFinder

--- ProductFinder.java
+++ ProductFinder.java
@@ -5,7 +5,13 @@ public class ProductFinder {
     private ArrayList listNumber;
     
     public ProductFinder(String number) {
-	
+	for (int i = 0; i < number.length(); i++) {
+	    if (number.charAt(i) == '\"')
+		continue;
+	    int element = 0;
+	    element = number.charAt(i) - '0';
+	    System.out.print(element);
+	}
     }
 
     public Integer findGreatestProductOf(Integer digits) {
